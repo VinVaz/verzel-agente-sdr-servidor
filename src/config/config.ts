a36aeae301dfce7756a1147311c77e02efcf858c
Vinícius Vaz <viniciusvaz97@gmail.com>
2025-10-27 11:53:16 -0300

    removed db from config

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,39 +8,27 @@ import messages from '../resources/lang/messages.json';
 dotenv.config();
 
 const envSchema = Joi.object({
-  NODE_ENV: Joi.string()
-    .valid('development', 'production', 'test')
-    .default('development'),
+  NODE_ENV: Joi.string().valid('development', 'production', 'test').default('development'),
 
-  SERVER_URL: Joi.string().uri().required(),
+  SERVER_URL: Joi.string().uri(),
   APP_PORT: Joi.number().integer().min(1).max(65535),
   TEST_APP_PORT: Joi.number().integer().min(1).max(65535),
 
   CLIENT_URL: Joi.string().uri().required(),
 
-  LOGGING_LEVEL: Joi.string()
-    .valid('error', 'warn', 'info', 'http', 'verbose', 'debug', 'silly')
-    .default('info'),
+  LOGGING_LEVEL: Joi.string().valid('error', 'warn', 'info', 'http', 'verbose', 'debug', 'silly').default('info'),
   LOGGING_MAX_SIZE: Joi.string().default('20m'),
   LOGGING_MAX_FILES: Joi.string().default('7d'),
   LOGGING_DATE_PATTERN: Joi.string().default('YYYY-MM-DD'),
   LOG_FILE_GENERATION_SUPPORT: Joi.boolean().truthy('true').falsy('false').default(true),
 
-  DB_CLIENT: Joi.string().required(),
-  DB_HOST: Joi.string().required(),
-  DB_PORT: Joi.number().integer().default(5432),
-  DB_NAME: Joi.string().required(),
-  TEST_DB_NAME: Joi.string(),
-  DB_USER: Joi.string().required(),
-  DB_PASSWORD: Joi.string().allow('').required(),
-
   npm_package_name: Joi.string().optional(),
-  npm_package_version: Joi.string().optional(),
+  npm_package_version: Joi.string().optional()
 }).unknown(true);
 
 const { error, value: env } = envSchema.validate(process.env, {
   abortEarly: false,
-  allowUnknown: true,
+  allowUnknown: true
 });
 
 if (error) {
@@ -65,25 +53,13 @@ export default {
   port: isTestEnvironment ? env.TEST_APP_PORT : env.APP_PORT,
   pagination: {
     page: 1,
-    maxRows: 20,
+    maxRows: 20
   },
   logging: {
     level: env.LOGGING_LEVEL,
     maxSize: env.LOGGING_MAX_SIZE,
     maxFiles: env.LOGGING_MAX_FILES,
     datePattern: env.LOGGING_DATE_PATTERN,
-    logFileGenarationSupport: env.LOG_FILE_GENERATION_SUPPORT,
-  },
-  db: {
-    client: env.DB_CLIENT,
-    connection: {
-      charset: 'utf8',
-      timezone: 'UTC',
-      host: env.DB_HOST,
-      port: env.DB_PORT,
-      database: isTestEnvironment ? env.TEST_DB_NAME : env.DB_NAME,
-      user: env.DB_USER,
-      password: env.DB_PASSWORD,
-    },
-  },
+    logFileGenarationSupport: env.LOG_FILE_GENERATION_SUPPORT
+  }
 };
